091d9a84fb501f33d450dcad7ed714860bcaa5a2
guneetgarg <guneetgarg@gmail.com>
2017-01-25 16:40:49 +0530

    initial

--- src/main/java/com/utilities/KeywordWrapper.java
+++ src/main/java/com/utilities/KeywordWrapper.java
@@ -3,6 +3,8 @@ package com.utilities;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class KeywordWrapper {
 	Logger log = LogManager.getLogger(KeywordWrapper.class);
@@ -10,18 +12,22 @@ public class KeywordWrapper {
 	static WebDriver driver;
 
 	public String openBrowser(String browser) {
-		log.info("");
-		String path = System.getProperty("user.dir");
+		log.info("Initializing browser" + browser);
+		String path = System.getProperty("user.dir") + "\\src\\main\\resources\\Drivers_executable\\";
 		if (browser.equalsIgnoreCase("Firefox")) {
 
 		} else if (browser.equalsIgnoreCase("Chrome")) {
-
+			System.setProperty("webdriver.chrome.driver", path + "chromedriver.exe");
+			driver = new ChromeDriver();
 		} else if (browser.equalsIgnoreCase("IE")) {
-
+			System.setProperty("webdriver.ie.driver", path + "IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 		} else {
 			log.info("Wrong Browser Name" + browser);
 		}
-		return "";
+		driver.manage().window().maximize();
+		log.info("Browser Open Successfully");
+		return "Pass";
 	}
 
 }
--- src/test/java/com/test/DriverScript.java
+++ src/test/java/com/test/DriverScript.java
@@ -3,6 +3,7 @@ package com.test;
 import java.lang.reflect.Method;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import com.utilities.KeywordWrapper;
 
@@ -11,6 +12,18 @@ public class DriverScript {
 	public static Method method[];
 	public static Method capturescreenShot_method;
 	public static KeywordWrapper keywords;
+	private static DriverScript DS;
+
+	private DriverScript() {
+
+	}
+	
+	public static DriverScript getDSInstance() {
+		if (DS == null) {
+			DS = new DriverScript();
+		}
+		return DS;
+	}
 
 	@BeforeClass
 	public void driverScript() throws Exception {
@@ -19,4 +32,10 @@ public class DriverScript {
 		capturescreenShot_method = keywords.getClass().getMethod("captureScreenshot", String.class, String.class);
 
 	}
+
+	@Test
+	public void execute() {
+		DS.getDSInstance();
+	}
+
 }
