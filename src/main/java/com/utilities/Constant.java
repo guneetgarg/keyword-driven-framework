f1d659ac77d50c93b54daa842e024d6aedff048f
guneetgarg <guneetgarg@gmail.com>
2017-01-28 23:19:27 +0530

    stage 2

--- src/main/java/com/utilities/Constant.java
+++ src/main/java/com/utilities/Constant.java
@@ -2,23 +2,35 @@ package com.utilities;
 
 public class Constant {
 
-	private String excelFilePath;
-	private String reportDir;
+	static private String excelFilePath;
+	static 	private String reportDir;
+	static private String screenShortDir;
 
+	public String getScreenShortDir() {
+		return screenShortDir;
+	}
+
+	public void setScreenShortDir(String screenShortDir) {
+		this.screenShortDir = System.getProperty("user.dir") + "\\Report\\ScreenShot_" + screenShortDir;
+	}
+
+	//////////////////////////////////////////////////////
 	public String getReportDir() {
 		return reportDir;
 	}
 
 	public void setReportDir(String reportDir) {
-		this.reportDir = reportDir;
+		this.reportDir = System.getProperty("user.dir") + "\\Report\\Report_" + reportDir;
 	}
 
 	///////////////////////////////////////////
 	public void setExcelUtil(String excelFilePath) {
 		this.excelFilePath = System.getProperty("user.dir") + "\\" + excelFilePath;
+		System.out.println("*******************"+excelFilePath);
 	}
 
 	public String getExcelUtil() {
+		System.out.println("--------------------------"+excelFilePath);
 		return excelFilePath;
 	}
 
