b423e504f178fbe35c091966bbbb933e1a142cdb
guneetgarg <guneetgarg@gmail.com>
2017-01-29 08:07:55 +0530

    Stage 6

--- src/main/java/com/utilities/KeywordWrapper.java
+++ src/main/java/com/utilities/KeywordWrapper.java
@@ -36,18 +36,22 @@ public class KeywordWrapper {
 	public String openBrowser(String browser) {
 		log.info("Initializing browser" + browser);
 		String path = System.getProperty("user.dir") + "\\src\\main\\resources\\Drivers_executable\\";
-		if (browser.equalsIgnoreCase("Firefox")) {
+		try {
+			if (browser.equalsIgnoreCase("Firefox")) {
 
-		} else if (browser.equalsIgnoreCase("Chrome")) {
-			System.setProperty("webdriver.chrome.driver", path + "chromedriver.exe");
-			driver = new ChromeDriver();
-		} else if (browser.equalsIgnoreCase("IE")) {
-			driver = new InternetExplorerDriver();
-		} else {
-			log.info("Wrong Browser Name" + browser);
-			return "Wrong Browser Name" + browser;
+			} else if (browser.equalsIgnoreCase("Chrome")) {
+				System.setProperty("webdriver.chrome.driver", path + "chromedriver.exe");
+				driver = new ChromeDriver();
+			} else if (browser.equalsIgnoreCase("IE")) {
+				driver = new InternetExplorerDriver();
+			} else {
+				log.info("Wrong Browser Name" + browser);
+				return "Wrong Browser Name" + browser;
+			}
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			return "Fail unable to open browser -- " + e;
 		}
-		driver.manage().window().maximize();
 		log.info("Browser Open Successfully");
 		return "Pass";
 	}
